449aceec098675fbf4f15ae7b32a6cbb8b1d47e2
Harold <harold.xing@treelab.co>
2021-02-10 10:45:06 +0800

    postMesaage

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default () => {
-  return <div>APP</div>;
+  const [message, setMessage] = useState("APP");
+  useEffect(() => {
+    const handler = (e: MessageEvent) => {
+      setMessage(e.data);
+    };
+    window.addEventListener("message", handler);
+    return () => window.removeEventListener("message", handler);
+  }, []);
+  return <div>{message}</div>;
 };
--- pages/iframe.tsx
+++ pages/iframe.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default () => {
-  return <iframe />;
+  const [message, setMessage] = useState("");
+  const iframeRef = useRef<HTMLIFrameElement>();
+  return (
+    <div>
+      <iframe ref={iframeRef} src="/app" />
+      <input
+        value={message}
+        onInput={(e) => setMessage(e.currentTarget.value)}
+      />
+      <button
+        onClick={() => {
+          iframeRef.current.contentWindow.postMessage(message, "*");
+        }}
+      >
+        发送
+      </button>
+    </div>
+  );
 };
